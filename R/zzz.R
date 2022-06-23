2a0c9ffffe0f1e844add8afdcbede61e22977f08
dyfan.jones <dyfan.jones@shopdirect.com>
2022-06-23 09:32:17 +0100

    feature: add logging to package initialisation

---
+++ R/zzz.R
@@ -0,0 +1,21 @@
+#' @import lgr
+"_PACKAGE"
+
+.onLoad <- function(libname, pkgname) {
+  # set package logs and don't propagate root logs
+  .logger = lgr::get_logger(name = "s3fs")$set_propagate(FALSE)
+
+  # # set logging layout
+  # .logger$add_appender(
+  #   lgr::AppenderConsole$new(
+  #     layout=sagemaker_log_layout()
+  #   )
+  # )
+
+  # set package logger
+  assign(
+    "LOGGER",
+    .logger,
+    envir = parent.env(environment())
+  )
+}
